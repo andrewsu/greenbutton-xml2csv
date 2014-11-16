7fe1bc2f7c3770b90a196faa17e30d5450b8273d
Andrew Su <andrew.su@gmail.com>
2014-11-15 21:49:00 -0800

    iterate over input file list

--- greenbutton-xml2csv.py
+++ greenbutton-xml2csv.py
@@ -2,20 +2,25 @@ import xml.etree.ElementTree as ET
 import datetime
 import sys
 
+if( len(sys.argv) < 2 ):
+   print "USAGE: python "+sys.argv[0]+" <inputfile> (<...>)"
+   sys.exit()
 
-tree = ET.parse('data/SDGE_Electric_60_Minute_11-01-2012_10-31-2013_20141115231229344.xml')
-root = tree.getroot()
 
-for child in root:
-   if( child.tag != '{http://www.w3.org/2005/Atom}entry'):
-      continue
-   if( len(child.findall('{http://www.w3.org/2005/Atom}title')) == 0):
-      continue
-   if( child.find('{http://www.w3.org/2005/Atom}title').text != 'Energy Usage'):
-      continue
-   for reading in child.findall('.//{http://naesb.org/espi}IntervalReading'):
-      timestart = reading.find('./{http://naesb.org/espi}timePeriod/{http://naesb.org/espi}start').text
-      timestartStr = datetime.datetime.fromtimestamp(int(timestart)).strftime('%Y-%m-%d %H:%M:%S')
-      value = reading.find('{http://naesb.org/espi}value').text
-      print timestartStr+","+ value
-
+for file in sys.argv[1:]:
+   tree = ET.parse(file)
+   root = tree.getroot()
+   
+   for child in root:
+      if( child.tag != '{http://www.w3.org/2005/Atom}entry'):
+         continue
+      if( len(child.findall('{http://www.w3.org/2005/Atom}title')) == 0):
+         continue
+      if( child.find('{http://www.w3.org/2005/Atom}title').text != 'Energy Usage'):
+         continue
+      for reading in child.findall('.//{http://naesb.org/espi}IntervalReading'):
+         timestart = reading.find('./{http://naesb.org/espi}timePeriod/{http://naesb.org/espi}start').text
+         timestartStr = datetime.datetime.fromtimestamp(int(timestart)).strftime('%Y-%m-%d %H:%M:%S')
+         value = reading.find('{http://naesb.org/espi}value').text
+         print timestartStr+","+ value
+   
